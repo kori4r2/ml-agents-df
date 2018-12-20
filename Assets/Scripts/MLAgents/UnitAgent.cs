c3ad94f3fb5a8c012a8252d6b6890f2cf1fea15f
unknown <italo.ts4895@gmail.com>
2018-12-20 16:41:21 +0900

    added base ml agents objects and scripts

---
+++ Assets/Scripts/MLAgents/UnitAgent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class UnitAgent : Agent {
+
+    Unit unit, ally, enemy1 = null, enemy2 = null, aux;
+
+	// Use this for initialization
+	void Start () {
+        unit = gameObject.GetComponent<Unit>();
+        GameObject[] units = GameObject.FindGameObjectsWithTag("Player");
+        aux = units[0].GetComponent<Unit>();
+        for (int i = 0; i < units.Length; i++, aux = units[i].GetComponent<Unit>()) {
+            if (aux.team == unit.team) {
+                if (aux.name.Equals(unit.team))
+                    ally = aux;
+            }else {
+                if (enemy1 == null)
+                    enemy1 = aux;
+                else
+                    enemy2 = aux;
+            }
+        }
+	}
+}
